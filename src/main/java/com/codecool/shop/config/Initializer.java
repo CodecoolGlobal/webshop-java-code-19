314743c729f25d9f10b65004a134afb27d5537ca
kincsooszfarkas <ofkincso99@gmail.com>
2020-05-07 16:42:17 +0200

    Modify design

--- src/main/java/com/codecool/shop/config/Initializer.java
+++ src/main/java/com/codecool/shop/config/Initializer.java
@@ -30,6 +30,8 @@ public class Initializer implements ServletContextListener {
         supplierDataStore.add(lenovo);
         Supplier apple = new Supplier("Apple", "Shitty overpriced electronical stuff");
         supplierDataStore.add(apple);
+        Supplier samsung = new Supplier("Samsung", "Samsung is great");
+        supplierDataStore.add(apple);
 
         //setting up a new product category
         ProductCategory tablet = new ProductCategory("Tablet", "Hardware", "A tablet computer, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display.");
@@ -39,8 +41,12 @@ public class Initializer implements ServletContextListener {
         productDataStore.add(new Product("Amazon Fire", 49.9f, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tablet, amazon));
         productDataStore.add(new Product("Lenovo IdeaPad Miix 700", 479, "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", tablet, lenovo));
         productDataStore.add(new Product("Amazon Fire HD 8", 89, "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", tablet, amazon));
-        productDataStore.add(new Product("Apple iPad", 500, "USD", "We all know that Apple is the best and most reliable (and overpriced)", tablet, apple));
-
+        productDataStore.add(new Product("Apple iPad Pro", 899.99f, "USD", "11-Inch edge-to-edge Liquid Retina display with Promotion, true Tone, and wide Color\n" +
+                "A12X Bionic chip with Neural Engine\n" +
+                "Face ID for secure authentication and Apple Pay\n" +
+                "12MP back camera, 7MP True Depth front camera", tablet, apple));
+        productDataStore.add(new Product("Samsung Galaxy Tab 4", 249.99f, "USD", "Android 4.4 Kit Kat OS, 1.2 GHz quad-core Qualcomm processor ; 16 GB Flash Memory, 1.5 GB RAM Memory and battery life is up to 10 hours", tablet, apple));
+        productDataStore.add(new Product("Samsung Galaxy Tab A", 243.88f, "USD", "Big sound for big entertainment. The perfect complement to a wide, immersive picture, Dolby Atmos Surround sound fills the room with cinematic clarity.", tablet, apple));
     }
 
 
