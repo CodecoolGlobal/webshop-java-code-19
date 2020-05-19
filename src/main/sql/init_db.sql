3ac439684ef26b7f5269442d635def6f0c099ab4
Alex Szabo <szabbo87@gmail.com>
2020-05-19 13:36:14 +0200

    init_db.sql done

--- src/main/sql/init_db.sql
+++ src/main/sql/init_db.sql
@@ -3,12 +3,12 @@ ALTER TABLE IF EXISTS ONLY products DROP CONSTRAINT IF EXISTS fk_category_id CAS
 ALTER TABLE IF EXISTS ONLY products DROP CONSTRAINT IF EXISTS fk_supplier_id CASCADE;
 ALTER TABLE IF EXISTS ONLY product_categories DROP CONSTRAINT IF EXISTS pk_category_id CASCADE;
 ALTER TABLE IF EXISTS ONLY suppliers DROP CONSTRAINT IF EXISTS pk_supplier_id CASCADE;
-DROP TABLE IF EXISTS public.suppliers;
-DROP TABLE IF EXISTS public.product_categories;
-DROP TABLE IF EXISTS public.products;
-DROP SEQUENCE IF EXISTS public.products_id_seq;
-DROP SEQUENCE IF EXISTS public.suppliers_id_seq;
-DROP SEQUENCE IF EXISTS public.product_categories_id_seq;
+DROP TABLE IF EXISTS suppliers;
+DROP TABLE IF EXISTS product_categories;
+DROP TABLE IF EXISTS products;
+DROP SEQUENCE IF EXISTS products_id_seq;
+DROP SEQUENCE IF EXISTS suppliers_id_seq;
+DROP SEQUENCE IF EXISTS product_categories_id_seq;
 CREATE TABLE IF NOT EXISTS "suppliers"(
     "id" serial NOT NULL,
     "name" text,
@@ -18,7 +18,7 @@ CREATE TABLE IF NOT EXISTS "suppliers"(
 ALTER TABLE ONLY suppliers
     ADD CONSTRAINT pk_supplier_id PRIMARY KEY (id);
 
-INSERT INTO public.suppliers values (1,'Amazon', 'Digital content and services');
+INSERT INTO suppliers values (1,'Amazon', 'Digital content and services');
 INSERT INTO suppliers values (2,'Lenovo', 'Computers');
 INSERT INTO suppliers values (3,'Apple', 'Designed in California');
 INSERT INTO suppliers values (4,'Samsung', 'Samsung is great');
@@ -26,7 +26,7 @@ INSERT INTO suppliers values (4,'Samsung', 'Samsung is great');
 SELECT pg_catalog.setval('suppliers_id_seq',4,true);
 
 CREATE TABLE IF NOT EXISTS "product_categories"(
-    "id" serial not null,
+    "id" serial NOT NULL,
     "name" text,
     "department" text,
     "description" text
@@ -40,7 +40,7 @@ INSERT INTO product_categories values (1,'tablet', 'Hardware','A tablet computer
 CREATE TABLE IF NOT EXISTS "products" (
     "id" serial NOT NULL,
     "name" text,
-    "default_price" integer,
+    "default_price" float,
     "currency_string" text,
     "description" text,
     "category_id" integer,
@@ -51,5 +51,5 @@ ALTER TABLE ONLY products
     ADD CONSTRAINT fk_supplier_id FOREIGN KEY (supplier_id) references suppliers(id),
     ADD CONSTRAINT fk_category_id FOREIGN KEY (category_id) references product_categories(id);
 
-INSERT INTO products values (1,'Amazon Fire', 49, 'USD','Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.', 1,1);
+INSERT INTO products values (1,'Amazon Fire', 49.99, 'USD','Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.', 1,1);
 SELECT pg_catalog.setval('products_id_seq',1,true);
